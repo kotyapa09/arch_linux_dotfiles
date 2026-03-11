930aa6330debf763aec748df9f4a602b367b2b16
kotyapa <135147372+kotyapa09@users.noreply.github.com>
2026-03-11 18:12:32 +0700

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,34 +2,16 @@
 # Create aliases
 alias la='ls -la'
 
-
 alias fetch='/home/user/bin/fetchs/fetch'
 alias sysfetch='/home/user/bin/fetchs/sysfetch'
 
-
 alias weather='/home/user/bin/weather'
 alias prognose='/home/user/bin/weather2'
-# TODO: Replace journal aliases after switching to OpenRC
-
-# Display critical errors
-alias syslog_emerg="sudo dmesg --level=emerg,alert,crit"
-
-# Output common errors
-alias syslog="sudo dmesg --level=err,warn"
-
-# Print logs from x server
-alias xlog='grep "(EE)\|(WW)\|error\|failed" ~/.local/share/xorg/Xorg.0.log'
-
-# Remove archived journal files until the disk space they use falls below 100M
-alias vacuum="journalctl --vacuum-size=100M"
-
-# Make all journal files contain no data older than 2 weeks
-alias vacuum_time="journalctl --vacuum-time=2weeks"
 
 set -U fish_greeting
 set fish_color_command green
-set -gx EDITOR micro
-set -gx VISUAL micro
+set -gx EDITOR vim
+set -gx VISUAL vim
 set -gx BROWSER /usr/bin/firefox
 
 
@@ -39,4 +21,4 @@ end
 
 sh /home/user/.config/fish/script.sh
 
-zoxide init fish
+zoxide init fish | source
